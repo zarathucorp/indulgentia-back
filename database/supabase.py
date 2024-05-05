a45bf7d8ce9e84e49101a59e1165a7d4f5977a4e
changwoolim <limcw@zarathu.com>
2024-05-05 23:04:41 +0900

    Move verify_user from database/supabase.py to func/auth/auth.py

--- database/supabase.py
+++ database/supabase.py
@@ -1,31 +1,7 @@
 from env import SUPABASE_URL, SUPABASE_KEY
 from supabase import create_client, Client
-from fastapi import Request
-from gotrue.errors import AuthApiError
+
 
 url: str = SUPABASE_URL
 key: str = SUPABASE_KEY
 supabase: Client = create_client(url, key)
-
-
-def verify_user(req: Request):
-    import urllib.parse
-    import json
-    from fastapi import HTTPException
-    SUPABASE_URL_REFERENCE_ID = SUPABASE_URL.replace(
-        "https://", "").replace(".supabase.co", "")
-    print(SUPABASE_URL_REFERENCE_ID)
-    SUPABASE_COOKIE = req.cookies.get(
-        f"sb-{SUPABASE_URL_REFERENCE_ID}-auth-token")
-    SUPABASE_COOKIE_DICT = json.loads(urllib.parse.unquote(
-        SUPABASE_COOKIE))
-    access_token = SUPABASE_COOKIE_DICT["access_token"]
-    try:
-        data = supabase.auth.get_user(access_token + "1")
-        user = data.user
-        return user.id
-    except AuthApiError:
-        raise HTTPException(status_code=401, detail="Unauthorized")
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail=f"Internal Server Error: {e}")
--- func/auth/auth.py
+++ func/auth/auth.py
@@ -7,8 +7,8 @@ import jwt
 from jwt import PyJWTError
 import secrets
 from datetime import datetime, timedelta
-
-
+from database.supabase import supabase
+from gotrue.errors import AuthApiError
 
 load_dotenv()
 
@@ -37,3 +37,25 @@ def decode_jwt(token: str) -> TokenPayload:
 def get_current_user(credentials: HTTPAuthorizationCredentials = Security(security)) -> TokenPayload:
     return decode_jwt(credentials.credentials)
 
+
+def verify_user(req: Request):
+    import urllib.parse
+    import json
+    from fastapi import HTTPException
+    SUPABASE_URL_REFERENCE_ID = SUPABASE_URL.replace(
+        "https://", "").replace(".supabase.co", "")
+    print(SUPABASE_URL_REFERENCE_ID)
+    SUPABASE_COOKIE = req.cookies.get(
+        f"sb-{SUPABASE_URL_REFERENCE_ID}-auth-token")
+    SUPABASE_COOKIE_DICT = json.loads(urllib.parse.unquote(
+        SUPABASE_COOKIE))
+    access_token = SUPABASE_COOKIE_DICT["access_token"]
+    try:
+        data = supabase.auth.get_user(access_token + "1")
+        user = data.user
+        return user.id
+    except AuthApiError:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    except Exception as e:
+        raise HTTPException(
+            status_code=500, detail=f"Internal Server Error: {e}")
