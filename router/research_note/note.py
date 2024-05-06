64578a3e1a5a7f2280fc55200312d058263717ba
changwoolim <limcw@zarathu.com>
2024-05-07 01:07:54 +0900

    Chore: minor fix

--- router/research_note/note.py
+++ router/research_note/note.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
 from pydantic import UUID4
-from database.supabase import verify_user
+from func.auth.auth import verify_user
 router = APIRouter(
     prefix="/note",
     responses={404: {"description": "Not found"}},
