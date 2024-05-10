fe1a4d0685d18a4bd745235d4a1418e4b00743ea
changwoolim <limcw@zarathu.com>
2024-05-10 15:42:28 +0900

    Add Azure presigned url function

--- cloud/azure/blob_storage.py
+++ cloud/azure/blob_storage.py
@@ -1,5 +1,25 @@
-from azure.storage.blob import BlobServiceClient
-from env import AZURE_STORAGE_CONNECTION_STRING
+from azure.storage.blob import BlobServiceClient, generate_blob_sas, BlobSasPermissions
+from env import AZURE_STORAGE_CONNECTION_STRING, DEFAULT_AZURE_CONTAINER_NAME
+from datetime import datetime, timedelta
 
-azure_client = BlobServiceClient.from_connection_string(
+azure_blob_client = BlobServiceClient.from_connection_string(
     conn_str=AZURE_STORAGE_CONNECTION_STRING)
+
+
+def generate_presigned_url(blob_name, container_name=DEFAULT_AZURE_CONTAINER_NAME, expiry_minutes=5):
+    account_name = azure_blob_client.account_name
+    blob_client = azure_blob_client.get_blob_client(
+        container=container_name, blob=blob_name)
+
+    sas_token = generate_blob_sas(
+        account_name=account_name,
+        container_name=container_name,
+        blob_name=blob_name,
+        account_key=azure_blob_client.credential.account_key,
+        permission=BlobSasPermissions(read=True),
+        expiry=datetime.utcnow() + timedelta(minutes=expiry_minutes)
+    )
+
+    blob_url = f"https://{account_name}.blob.core.windows.net/{
+        container_name}/{blob_name}?{sas_token}"
+    return blob_url
