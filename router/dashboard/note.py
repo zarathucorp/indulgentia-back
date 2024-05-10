9c66f3c50bc7865d2b07e1c25ef72eca2a8fa579
changwoolim <limcw@zarathu.com>
2024-05-10 15:57:08 +0900

    Add endpoint /dashboard/note/file/{note_id}

--- router/dashboard/note.py
+++ router/dashboard/note.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.responses import JSONResponse
 from pydantic import UUID4
-
+from cloud.azure.blob_storage import generate_presigned_url
 from database import schemas
 from func.dashboard.crud.note import *
 
@@ -25,6 +25,7 @@ async def add_note(req: Request, note: schemas.NoteCreate):
         "data": res["content"]
     })
 
+
 @router.get("/{note_id}")
 async def get_note(req: Request, note_id: str):
     res = read_note(note_id)
@@ -37,6 +38,7 @@ async def get_note(req: Request, note_id: str):
         "status": "succeed",
         "data": res["content"]
     })
+
 
 @router.get("/list/{bucket_id}")
 async def get_note_list(req: Request, bucket_id: str):
@@ -51,6 +53,7 @@ async def get_note_list(req: Request, bucket_id: str):
         "data": res["content"]
     })
 
+
 @router.put("/")
 async def change_note(req: Request, note: schemas.NoteUpdate):
     res = update_note(note)
@@ -64,6 +67,7 @@ async def change_note(req: Request, note: schemas.NoteUpdate):
         "data": res["content"]
     })
 
+
 @router.delete("/{note_id}")
 async def drop_note(req: Request, note_id: str):
     res = delete_note(note_id)
@@ -76,3 +80,13 @@ async def drop_note(req: Request, note_id: str):
         "status": "succeed",
         "data": res["content"]
     })
+
+
+@router.get("/file/{note_id}")
+async def get_note_file(req: Request, note_id: str):
+    # Auth 먼저 해야함
+    try:
+        url = generate_presigned_url(note_id + ".pdf")
+        return JSONResponse(content={"status": "succeed", "url": url})
+    except Exception as e:
+        return JSONResponse(status_code=400, content={"status": "failed", "message": str(e)})
