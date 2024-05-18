6806672cd23f4a3951f0e1a1f81efcb06bb10d71
changwoolim <limcw@zarathu.com>
2024-05-18 19:03:01 +0900

    temporarily added breadcrumb endpoint

--- router/dashboard/dashboard.py
+++ router/dashboard/dashboard.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends, HTTPException
+
+from fastapi import APIRouter, Depends, HTTPException, Request
 from . import project
 from . import bucket
 from . import note
@@ -10,3 +11,41 @@ router = APIRouter(
 router.include_router(project.router)
 router.include_router(bucket.router)
 router.include_router(note.router)
+
+
+@router.get("/breadcrumb/note/{note_id}")
+async def get_breadcrumb(req: Request, note_id: str):
+    from pydantic import UUID4
+    from func.auth.auth import verify_user
+    from database.supabase import supabase
+    from fastapi.responses import JSONResponse
+    user: UUID4 = verify_user(req)
+    if not user:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    data, count = supabase.rpc(
+        "note_breadcrumb_data", {"note_id": note_id}).execute()
+    if not data[1]:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    return JSONResponse(content={
+        "status": "succeed",
+        "data": data[1][0]
+    })
+
+
+@router.get("/breadcrumb/bucket/{bucket_id}")
+async def get_breadcrumb(req: Request, bucket_id: str):
+    from pydantic import UUID4
+    from func.auth.auth import verify_user
+    from database.supabase import supabase
+    from fastapi.responses import JSONResponse
+    user: UUID4 = verify_user(req)
+    if not user:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    data, count = supabase.rpc(
+        "bucket_breadcrumb_data", {"bucket_id": bucket_id}).execute()
+    if not data[1]:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    return JSONResponse(content={
+        "status": "succeed",
+        "data": data[1][0]
+    })
