834ed1304131c80a26b8adf51f69d6f90a3d4114
Jaewoong Heo <koolerjaebee@gmail.com>
2024-06-12 12:04:34 +0900

    refactor: change generate_pdf func to async for pdf write performance

--- router/dashboard/note.py
+++ router/dashboard/note.py
@@ -115,8 +115,8 @@ async def add_note(req: Request,
         "bucket_breadcrumb_data", {"bucket_id": str(bucket_id)}).execute()
     if not breadcrumb_data[1]:
         raise_custom_error(500, 250)
-    pdf_res = generate_pdf(title=title, username=username,
-                           note_id=str(note_id), description=description, files=files, contents=contents, project_title=breadcrumb_data[1][0].get("project_title"), bucket_title=breadcrumb_data[1][0].get("bucket_title"), signature_url=url)
+    pdf_res = await generate_pdf(title=title, username=username,
+                                 note_id=str(note_id), description=description, files=files, contents=contents, project_title=breadcrumb_data[1][0].get("project_title"), bucket_title=breadcrumb_data[1][0].get("bucket_title"), signature_url=url)
     await sign_pdf(pdf_res)
     signed_pdf_res = f"func/dashboard/pdf_generator/output/{note_id}_signed.pdf"
     try:
@@ -166,7 +166,7 @@ async def add_note(req: Request,
 # update
 
 
-@router.put("/{note_id}", tags=["note"])
+@ router.put("/{note_id}", tags=["note"])
 async def change_note(req: Request, note: schemas.NoteUpdate):
     user: UUID4 = verify_user(req)
     if not user:
@@ -185,7 +185,7 @@ async def change_note(req: Request, note: schemas.NoteUpdate):
 # delete
 
 
-@router.delete("/{note_id}", tags=["note"])
+@ router.delete("/{note_id}", tags=["note"])
 async def drop_note(req: Request, note_id: str):
     try:
         uuid.UUID(note_id)
@@ -226,7 +226,7 @@ async def drop_note(req: Request, note_id: str):
 """
 
 
-@router.get("/file/{note_id}", tags=["note"])
+@ router.get("/file/{note_id}", tags=["note"])
 async def get_note_file(req: Request, note_id: str):
     # Auth 먼저 해야함
     user: UUID4 = verify_user(req)
@@ -242,7 +242,7 @@ async def get_note_file(req: Request, note_id: str):
         # return JSONResponse(status_code=400, content={"status": "failed", "message": str(e)})
 
 
-@router.get("/{note_id}/breadcrumb", tags=["note"])
+@ router.get("/{note_id}/breadcrumb", tags=["note"])
 async def get_breadcrumb(req: Request, note_id: str):
     try:
         uuid.UUID(note_id)
@@ -261,7 +261,7 @@ async def get_breadcrumb(req: Request, note_id: str):
     })
 
 
-@router.get("/{note_id}/timestamp", tags=["note"])
+@ router.get("/{note_id}/timestamp", tags=["note"])
 def get_note_timestamp(req: Request, note_id: str):
     try:
         uuid.UUID(note_id)
