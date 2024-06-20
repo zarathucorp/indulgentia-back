c4b88044b0e64186a688ab491dfb5d20ddd54db2
Jaewoong Heo <koolerjaebee@gmail.com>
2024-06-20 13:13:26 +0900

    feat: add user signature upload API

--- router/user/settings.py
+++ router/user/settings.py
@@ -18,7 +18,8 @@ router = APIRouter(
 # Temp
 SIGNATURE_AZURE_CONTAINER_NAME = DEFAULT_AZURE_CONTAINER_NAME
 
-ALLOWED_EXTENSIONS = {'png'}
+ALLOWED_EXTENSIONS = ['png', 'jpg', 'jpeg', 'gif',
+                      'svg', 'bmp', 'webp', 'tiff', 'jfif', 'pjpeg', 'pjp']
 
 
 def allowed_file(filename):
@@ -46,37 +47,34 @@ async def get_signature(req: Request):
         # return JSONResponse(status_code=400, content={"status": "failed", "message": str(e)})
 
 
-# # file upload 방식
-# @router.post("/signature", tags=["settings"])
-# async def add_signature(req: Request, file: UploadFile = File(...)):
-#     # user: UUID4 = verify_user(req)
-#     # if not user:
-#     #     raise HTTPException(status_code=401, detail="Unauthorized")
+# file upload 방식
+@router.post("/signature/file", tags=["settings"])
+async def add_signature_file(req: Request, file: UploadFile = File(...)):
+    user: UUID4 = verify_user(req)
+    if not user:
+        raise_custom_error(403, 213)
 
-#     # testing user
-#     user = UUID("6a423db5-8a34-4153-9795-c6f058020445", version=4)
+    if not allowed_file(file.filename):
+        raise_custom_error(422, 240)
+    data = await file.read()
+    blob_extention = file.filename.split(".")[-1]
+    blob_name = str(user) + "." + blob_extention
+    res = upload_blob(data, blob_name)
+    if res:
+        data, count = supabase.table("user_setting").update(
+            {"has_signature": True}).eq("id", user).execute()
+        if not data[1]:
+            raise_custom_error(500, 220)
+        return JSONResponse(content={
+            "status": "succeed",
+            "data": {
+                "has_signature": data[1].get("has_signature"),
+                "signature_type": data[1].get("signature_type")
+            }
+        })
+    else:
+        raise_custom_error(500, 311)
 
-#     if not allowed_file(file.filename):
-#         raise HTTPException(status_code=400, detail="Invalid file extension. Allowed extensions are png, jpg, jpeg")
-#     data = await file.read()
-#     blob_extention = file.filename.split(".")[-1]
-#     blob_name = str(user) + "." + blob_extention
-#     res = upload_blob(data, blob_name)
-#     if res:
-#         data, count = supabase.table("user_setting").update({"has_signature": True, "signature_type": blob_extention}).eq("id", user).execute()
-#         if not data[1]:
-#             raise HTTPException(status_code=500, detail="Internal Server Error")
-#         return JSONResponse(content={
-#             "status": "succeed",
-#             "data": {
-#                 "has_signature": data[1].get("has_signature"),
-#                 "signature_type": data[1].get("signature_type")
-#             }
-#         })
-#     else:
-#         return JSONResponse(content={
-#             "status": "failed",
-#         })
 
 @router.post("/signature", tags=["settings"])
 # async def add_signature(req: Request, file: UploadFile = File(...)):
