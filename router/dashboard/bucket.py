ed34877f0852511d5ef351d9c25352a9c24b8e9f
Jaewoong Heo <koolerjaebee@gmail.com>
2024-07-10 05:11:28 +0000

    fix: fix typo

--- router/dashboard/bucket.py
+++ router/dashboard/bucket.py
@@ -2,12 +2,21 @@ from fastapi import APIRouter, Depends, HTTPException, Request
 from fastapi.responses import JSONResponse
 from pydantic import UUID4
 import uuid
+import hashlib
+from datetime import datetime
+from pytz import timezone
 
 from database import schemas
 from func.dashboard.crud.bucket import *
 from func.auth.auth import *
+from cloud.azure.blob_storage import *
+from cloud.azure.confidential_lendger import write_ledger, read_ledger, get_ledger_receipt
 from func.error.error import raise_custom_error
 from func.user.team import validate_user_in_premium_team
+from func.github.fetch import fetch_github_data
+from func.dashboard.pdf_generator.pdf_generator import generate_pdf_using_markdown
+from func.note_handling.pdf_sign import sign_pdf
+from func.dashboard.crud.note import *
 
 
 router = APIRouter(
@@ -212,9 +221,134 @@ async def connect_github_repository(req: Request, bucket_id: str, newRepo: schem
         raise_custom_error(401, 310)
     check_gitrepo_exists_result = supabase.rpc("check_gitrepo_exists", {"g_bucket_id": bucket_id, "g_repo_url": newRepo.repo_url}).execute().data
     if check_gitrepo_exists_result:
+        print(check_gitrepo_exists_result)
         print("here")
         raise_custom_error(401, 330)
     data = create_connected_gitrepo(newRepo, user)
+
+    # 이전 github commit | issue | PR 내역 note에 저장
+    user_data, count = supabase.table("user_setting").select("github_token").eq(
+        "is_deleted", False).eq("id", user).execute()
+    if not user_data[1]:
+        raise_custom_error(500, 231)
+
+    commit_responses = fetch_github_data(user_data[1][0].get("github_token"), newRepo.git_username, newRepo.git_repository, "commits")
+    issue_responses = fetch_github_data(user_data[1][0].get("github_token"), newRepo.git_username, newRepo.git_repository, "issues")
+    pr_responses = fetch_github_data(user_data[1][0].get("github_token"), newRepo.git_username, newRepo.git_repository, "pulls")
+    commit_markdown = "# Commit Details\n\n"
+    issue_markdown = ""
+    issue_action = {
+        "open": "Issue opened",
+        "closed": "Issue closed",
+        "all": "Issue all"
+    }
+    pr_markdown = ""
+    pr_action = {
+        "open": "Pull Request Event: opened",
+        "closed": "Pull Request Event: closed",
+        "all": "Pull Request Event: all"
+    }
+    for res in commit_responses:
+        commit_markdown += f"## Commit by {res.get('commit').get('author').get('name')}\n"
+        commit_markdown += f"- **SHA: **{res.get('sha')}\n"
+        commit_markdown += f"- **Message: **{res.get('commit').get('message')}\n"
+        commit_markdown += f"- **URL: **[Commit Link]({res.get('html_url')})\n\n"
+    for res in issue_responses:
+        issue_markdown += f"## {issue_action.get(res.get('state'))}\n"
+        issue_markdown += f"- Title: {res.get('title')}\n"
+        issue_markdown += f"- Description: {res.get('body', 'No description')}\n"
+        issue_markdown += f"- Author: {res.get('user').get('login')}\n"
+        issue_markdown += f"- URL: [Link]({res.get('html_url')})\n\n"
+    for res in pr_responses:
+        pr_markdown += f"## {pr_action.get(res.get('state'))}\n"
+        pr_markdown += f"- PR Number: {res.get('number')}\n"
+        pr_markdown += f"- Title: {res.get('title')}\n"
+        pr_markdown += f"- Author: {res.get('user').get('login')}\n"
+        pr_markdown += f"- State: {res.get('state')}\n"
+        pr_markdown += f"- Created At: {res.get('created_at')}\n"
+        if res.get('state') == "closed" and res.get('merged'):
+            pr_markdown += f"- Merged: Yes\n"
+        elif res.get('state') == "closed":
+            pr_markdown += f"- Merged: No\n"
+    markdown_content = commit_markdown + issue_markdown + pr_markdown
+
+    # res_list = []
+    # for idx, row in enumerate(data[1]):
+    note_id = uuid.uuid4()
+    note_id_string = str(note_id)
+    if not validate_user_in_premium_team(user):
+        # break
+        raise_custom_error(401, 820)
+    user_data, count = supabase.table("user_setting").select(
+        "*").eq("is_deleted", False).eq("id", user).execute()
+    has_signature = user_data[1][0].get("has_signature")
+    if has_signature:
+        url = generate_presigned_url(
+            str(user_data[1][0].get("id")) + ".png")
+    else:
+        url = None
+    first_name = user_data[1][0].get("first_name")
+    last_name = user_data[1][0].get("last_name")
+    if not first_name and not last_name:
+        raise_custom_error(401, 121)
+        # username = "No Name"
+    else:
+        username = last_name + " " + first_name
+
+    breadcrumb_data, count = supabase.rpc(
+        "bucket_breadcrumb_data", {"bucket_id": bucket_id}).execute()
+    if not breadcrumb_data[1]:
+        raise_custom_error(500, 250)
+
+    pdf_res = await generate_pdf_using_markdown(note_id=note_id_string, markdown_content=markdown_content, project_title=breadcrumb_data[1][0].get("project_title"), bucket_title=breadcrumb_data[1][0].get("bucket_title"), author=username, signature_url=url)
+    await sign_pdf(pdf_res)
+    signed_pdf_res = f"func/dashboard/pdf_generator/output/{note_id_string}_signed.pdf"
+    SOURCE_PATH = "func/dashboard/pdf_generator"
+    try:
+        # upload pdf
+        with open(signed_pdf_res, "rb") as f:
+            pdf_data = f.read()
+    except Exception as e:
+        print(e)
+        # delete result pdf
+        if os.path.isfile(f"{SOURCE_PATH}/output/{note_id_string}.pdf"):
+            os.unlink(f"{SOURCE_PATH}/output/{note_id_string}.pdf")
+            print(f"{SOURCE_PATH}/output/{note_id_string}.pdf deleted")
+        raise_custom_error(500, 120)
+    # raise Exception("test")
+    upload_blob(pdf_data, note_id_string + ".pdf")
+    pdf_hash = hashlib.sha256(pdf_data).hexdigest()
+    ledger_result = write_ledger(
+        {"id": note_id_string, "hash": pdf_hash})
+    try:
+        os.unlink(f"{SOURCE_PATH}/output/{note_id_string}.pdf")
+        print(f"{SOURCE_PATH}/output/{note_id_string}.pdf deleted")
+        os.unlink(f"{SOURCE_PATH}/output/{note_id_string}_signed.pdf")
+        print(f"{SOURCE_PATH}/output/{note_id_string}_signed.pdf deleted")
+    except Exception as e:
+        print(e)
+        raise_custom_error(500, 130)
+
+    transaction_id = ledger_result.get("transactionId")
+    current_time = datetime.now(
+        timezone('Asia/Seoul')).strftime("%Y-%m-%d %H:%M:%S")
+    note = schemas.NoteCreate(
+        id=note_id,
+        bucket_id=uuid.UUID(bucket_id),
+        user_id=user,
+        # title=f"{current_time}에 생성된 {GithubMarkdownRequest.eventType} 노트(Github)",
+        title=f"{current_time} 이전에 생성된 모든 Commit | PR | Issue 노트(Github)",
+        timestamp_transaction_id=transaction_id,
+        # file_name=f"{current_time}에 생성된 {GithubMarkdownRequest.eventType} 노트(Github)",
+        file_name=f"{current_time} 이전에 생성된 모든 Commit | PR | Issue 노트(Github)",
+        is_github=True,
+        github_type="Commit",
+        github_link=newRepo.repo_url,
+        pdf_hash=pdf_hash
+    )
+    res = create_note(note)
+    print(res)
+
     return JSONResponse(content={
         "status": "succeed",
         "data": data
