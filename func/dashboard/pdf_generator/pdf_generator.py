9d4c61a6e34a15185f561dcce8c1caee1c0a19a6
Jaewoong Heo <koolerjaebee@gmail.com>
2024-12-04 18:45:22 +0900

    feat: change description text to markdown and add validation for description

--- func/dashboard/pdf_generator/pdf_generator.py
+++ func/dashboard/pdf_generator/pdf_generator.py
@@ -12,77 +12,10 @@ import subprocess
 import re
 import asyncio
 import aiofiles
+from markdown import markdown
 from func.error.error import raise_custom_error
-
+from datetime import datetime
 import time
-# def generate_pdf(user_id:str, files=List[UploadFile], descriptions=List[str]):
-#     class PDF(FPDF):
-#         def header(self):
-#             self.add_font("Pretendard", style="B", fname="Pretendard-Bold.ttf")
-#             self.set_font("Pretendard", "B", 12)
-#             self.cell(0, 10, user_id, 0, 1, "C")
-#             self.ln(10)
-
-#         def footer(self):
-#             self.add_font("Pretendard", style="I", fname="Pretendard-Thin.ttf")
-#             self.set_y(-15)
-#             self.set_font("Pretendard", "I", 8)
-#             self.cell(0, 10, f"Page {self.page_no()}", 0, 0, "R")
-
-#     pdf = PDF()
-#     pdf.add_font("Pretendard", style="", fname="Pretendard-Regular.ttf")
-#     pdf.set_font("Pretendard", size=12)
-
-#     if len(files) != len(descriptions):
-#         raise HTTPException(status_code=400, detail="Number of files and descriptions do not match")
-
-#     num_pages = len(descriptions)
-
-#     for idx in num_pages:
-#         description = descriptions[idx] if descriptions[idx] else None
-#         description = description.encode("utf-8")[:800].decode("utf-8", "ignore") if description else None # Limit description to 800 characters
-
-#         raw_image = files[idx].file.read() if files[idx] else None
-
-#         pdf.add_page()
-
-#         if raw_image:
-#             max_image_height = pdf.h * 1/2
-#             max_image_width = pdf.w - 20  # 10 points margin on each side
-#             image_stream = io.BytesIO(raw_image)
-#             image = Image.open(image_stream)
-#             image_height = image.height * max_image_height / image.width
-#             image_width = image.width * max_image_height / image.height
-
-#             if image_height > max_image_height:
-#                 image_height = max_image_height
-#                 image_width = image.width * image_height / image.height
-
-#             if image_width > max_image_width:
-#                 image_width = max_image_width
-#                 image_height = image.height * image_width / image.width
-
-#             image_y = (pdf.h - image_height) / 2  # Center the image
-#             image_x = (pdf.w - image_width) / 2
-#             pdf.image(raw_image, x=image_x, y=image_y, h=image_height, w=image_width)
-
-#             if description:
-#                 pdf.set_y(image_y + image_height + 10)
-#                 pdf.multi_cell(0, 10, description)
-#         elif description:
-#             pdf.set_y(10)  # Set description at the top
-#             pdf.multi_cell(0, 10, description)
-#         else:
-#             continue  # Skip to the next page if both image and description are None
-
-#     res = pdf.output()
-
-#     print(res)
-#     print(type(res))
-
-#     return res
-
-# MS office & HWP to PDF
 
 
 def convert_doc_to_pdf(source_path: str, file_name: str, extension: str):
@@ -154,11 +87,38 @@ def count_sections_and_split(markdown_content):
 
 
 def create_intro_page(title: str, author: str, description: str | None, SOURCE_PATH: str, note_id: str, project_title: str, bucket_title, signature_url: str | None = None):
-    from datetime import datetime
+    # 마크다운 텍스트 크기 정보 (FPDF2)
+    # h1 폰트 크기: 24
+    # h1 y길이: 14
+    # h1 1줄 글자수: 43
+    #
+    # h2 폰트 크기: 20
+    # h2 y길이: 12
+    # h2 1줄 글자수: 49
+    #
+    # h3 폰트 크기: 16
+    # h3 y길이: 10.1
+    # h3 1줄 글자수: 68 (61로 취급할 것)
+    #
+    # h4 폰트 크기: 14
+    # h4 y길이: 9.1
+    # h4 1줄 글자수: 61
+    #
+    # p 폰트 크기: 12
+    # p y길이: 6.5
+    # p 1줄 글자수: 98
+    # code y길이: 6.5
+    # table y길이: 11 / 줄
+    # list y길이: 6.5 / 줄
+    #
+    # page height: 297
+    # 타이틀 height: 40
+    # 서명 height: 50
+    # 가용 height: 200 가정 (실제값 207)
 
     try:
         pdf = FPDF()
-        pdf.set_auto_page_break(auto=True, margin=5)
+        pdf.set_auto_page_break(auto=False, margin=5)
 
         date = datetime.now().strftime("%Y-%m-%d")
         date_kor = datetime.now().strftime("%Y년 %m월 %d일")
@@ -166,6 +126,11 @@ def create_intro_page(title: str, author: str, description: str | None, SOURCE_P
 
         pdf.add_font("Pretendard", style="",
                      fname=f"{SOURCE_PATH}/Pretendard-Regular.ttf")
+        pdf.add_font("Pretendard", style="B",
+                     fname=f"{SOURCE_PATH}/Pretendard-Bold.ttf")
+        pdf.add_font("PretendardB", style="",
+                     fname=f"{SOURCE_PATH}/Pretendard-Bold.ttf")
+
         pdf.set_font("Pretendard", size=24)
         pdf.cell(200, 10, text=title, ln=True, align='C')
         pdf.set_y(pdf.get_y()+10)
@@ -174,14 +139,30 @@ def create_intro_page(title: str, author: str, description: str | None, SOURCE_P
         pdf.set_y(pdf.get_y())
         pdf.set_font_size(12)
         pdf.cell(200, 10, text="Description", ln=True, align='L')
+
         if description:
-            # # Limit description to 2000 characters
-            # description = description.encode(
-            #     "utf-8")[:2000].decode("utf-8", "ignore") if description else None
-            description = description[:1000]
-            print(description)
-            pdf.set_font_size(12)
-            pdf.multi_cell(0, 10, description)
+            html_intro = markdown(
+                description, extensions=['fenced_code', 'markdown.extensions.tables'])
+            # <hr /> 태그를 수평선으로 변환하기 위해 리스트로 변환
+            html_intro = re.sub(r"<hr\s*/?>", "<hr>", html_intro)
+            print("html_intro", html_intro)
+            print("line break count", html_intro.count("\n"))
+            html_intro_list = html_intro.split(r"<hr>")
+
+            for html in html_intro_list:
+                pdf.write_html(html, tag_styles={
+                    # "h1": FontFace(family="PretendardB", color=(0, 0, 0), size_pt=24),
+                    "h1": FontFace(family="PretendardB", color=(0, 0, 0), size_pt=20),
+                    # "h2": FontFace(family="PretendardB", color=(0, 0, 0), size_pt=20),
+                    "h2": FontFace(family="PretendardB", color=(0, 0, 0), size_pt=18),
+                    "h3": FontFace(family="PretendardB", color=(0, 0, 0), size_pt=16),
+                    "h4": FontFace(family="PretendardB", color=(0, 0, 0), size_pt=14),
+                    "a": FontFace(family="Pretendard", color=(0, 0, 255), emphasis=None),
+                    "code": FontFace(family="Pretendard", color=(120, 120, 120), size_pt=12),
+                }, li_prefix_color=(0, 0, 0), ul_bullet_char=u"\u2022")
+                if html != html_intro_list[-1]:
+                    x1, y1, x2, y2 = pdf.get_x(), pdf.get_y() + 2, pdf.w - 10, pdf.get_y() + 2
+                    pdf.line(x1, y1, x2, y2)
 
         pdf.set_font_size(10)
         pdf.set_y(pdf.h - 50)
@@ -282,8 +263,6 @@ async def generate_pdf(title: str, username: str, note_id: str, description: str
     delete_old_files(f"{SOURCE_PATH}/output")
 
     # Intro PDF
-    print(description)
-    print(files)
     create_intro_page(title, username, description,
                       SOURCE_PATH, note_id, project_title, bucket_title, signature_url)
 
@@ -392,7 +371,6 @@ async def generate_pdf(title: str, username: str, note_id: str, description: str
 async def generate_pdf_using_markdown(note_id: str, markdown_content: str, project_title: str, bucket_title: str, author: str, signature_url: str | None = None):
     from datetime import datetime
     from fpdf import HTMLMixin
-    from markdown import markdown
     SOURCE_PATH = "func/dashboard/pdf_generator"
 
     # delete old files
@@ -527,122 +505,3 @@ async def generate_pdf_using_markdown(note_id: str, markdown_content: str, proje
         raise_custom_error(500, 110)
 
     return f"{SOURCE_PATH}/output/{note_id}"
-
-# async def generate_pdf(title: str, username: str, note_id: str, description: str | None, files: List[Union[UploadFile, None]], contents: List[Union[bytes, None]], project_title: str, bucket_title: str, signature_url: str | None = None):
-#     SOURCE_PATH = "func/dashboard/pdf_generator"
-#     DOC_EXTENSIONS = ["doc", "docx", "hwp",
-#                       "hwpx", "ppt", "pptx", "xls", "xlsx"]
-#     IMAGE_EXTENSIONS = ["png", "jpg", "jpeg", "bmp"]
-#     AVAILABLE_EXTENSIONS = DOC_EXTENSIONS + IMAGE_EXTENSIONS + ["pdf"]
-#     A4_SIZE = (595, 842)
-
-#     # Intro PDF
-#     print(description)
-#     print(files)
-#     create_intro_page(title, username, description,
-#                       SOURCE_PATH, note_id, project_title, bucket_title, signature_url)
-
-#     async def process_file(file, idx):
-#         extension = file.filename.split(".")[-1]
-#         filename = f"{note_id}_{idx}"
-#         try:
-#             with open(f"{SOURCE_PATH}/input/{filename}.{extension}", 'wb') as f:
-#                 f.write(contents[idx])
-#                 print(f"{SOURCE_PATH}/input/{filename}.{extension} saved")
-
-#             if extension in DOC_EXTENSIONS:
-#                 res = convert_doc_to_pdf(SOURCE_PATH, filename, extension)
-#                 print(f"{SOURCE_PATH}/output/{res} saved")
-#             elif extension in IMAGE_EXTENSIONS:
-#                 image = Image.open(
-#                     f"{SOURCE_PATH}/input/{filename}.{extension}")
-#                 image.thumbnail(A4_SIZE)
-#                 if image.mode == "RGBA":
-#                     image.load()
-#                     background = Image.new(
-#                         "RGB", image.size, (255, 255, 255))
-#                     background.paste(image, mask=image.split()[3])
-#                     background.save(f"{SOURCE_PATH}/output/{filename}.pdf")
-#                 else:
-#                     image.save(f"{SOURCE_PATH}/output/{filename}.pdf")
-#                 print(f"{SOURCE_PATH}/output/{filename}.pdf saved")
-#             else:
-#                 # pdf file
-#                 with open(f"{SOURCE_PATH}/output/{filename}.{extension}", 'wb') as f:
-#                     f.write(contents[idx])
-#                     print(f"{SOURCE_PATH}/output/{filename}.pdf saved")
-#         except FileNotFoundError as e:
-#             print(e)
-#             # 이미지 pdf 없음
-#             raise_custom_error(500, 430)
-#         except ValueError as e:
-#             print(e)
-#             # 이미지 pdf 변환 오류
-#             raise_custom_error(500, 430)
-#         except UnidentifiedImageError as e:
-#             print(e)
-#             # PIL 범용 에러
-#             raise_custom_error(500, 430)
-#         except OSError as e:
-#             print(e)
-#             # file 읽기/쓰기 오류
-#             raise_custom_error(500, 110)
-#         except Exception as e:
-#             print(e)
-#             # 그외 에러
-#             raise_custom_error(500, 400)
-
-#     if files:
-#         if not all([file.filename.split(".")[-1] in AVAILABLE_EXTENSIONS for file in files]):
-#             raise HTTPException(
-#                 status_code=422, detail="Unprocessable file extension")
-#         import time
-#         start = time.time()
-#         print("start time", start)
-#         tasks = [process_file(file, idx) for idx, file in enumerate(files)]
-#         await asyncio.gather(*tasks)
-#         end = time.time()
-#         print("end_time", end)
-#         print("time elapsed", end-start)
-
-#     # merge pdfs
-#     try:
-#         pdfs = [f"{SOURCE_PATH}/output/{note_id}_intro.pdf"]
-#         if files:
-#             pdfs = pdfs + \
-#                 [f"{SOURCE_PATH}/output/{note_id}_{idx}.pdf" for idx in range(
-#                     len(files))]
-#         print(pdfs)
-#         pdfmerge(pdfs, f"{SOURCE_PATH}/output/{note_id}.pdf")
-#         print(f"{SOURCE_PATH}/output/{note_id}.pdf saved")
-#     except Exception as e:
-#         print(e)
-#         raise_custom_error(500, 440)
-#         raise HTTPException(
-#             status_code=500, detail="Failed to merge pdfs")
-
-#     # delete files
-#     try:
-#         os.unlink(f"{SOURCE_PATH}/output/{note_id}_intro.pdf")
-#         print(f"{SOURCE_PATH}/output/{note_id}_intro.pdf deleted")
-#         if files:
-#             for idx, file in enumerate(files):
-#                 file_input_path = os.path.join(
-#                     SOURCE_PATH + "/input/", f"{note_id}_{idx}.{file.filename.split('.')[-1]}")
-#                 file_output_path = os.path.join(
-#                     SOURCE_PATH + "/output/", f"{note_id}_{idx}.pdf")
-#                 if os.path.isfile(file_input_path):
-#                     os.unlink(file_input_path)
-#                     print(f"{file_input_path} deleted")
-#                 if os.path.isfile(file_output_path):
-#                     os.unlink(file_output_path)
-#                     print(f"{file_output_path} deleted")
-#     except Exception as e:
-#         print(e)
-#         raise_custom_error(500, 130)
-#     print("Success!")
-
-#     return f"{SOURCE_PATH}/output/{note_id}"
-
-# # # testing
-# # convert_doc_to_pdf("func/dashboard/pdf_generator", "3d4256d9-20e8-4acc-9c51-42c90b4456ab_0", "docx")
