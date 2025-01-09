280270bb8bbccc2dd2940c05448aced1a6a38bc5
Jaewoong Heo <koolerjaebee@gmail.com>
2025-01-09 18:31:28 +0900

    feat: add simple logging config

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-
 import uvicorn
+from uvicorn.config import LOGGING_CONFIG
 
 from router.auth import auth
 from router.admin import admin
@@ -13,12 +13,12 @@ from router.payment import payment
 from router.validate import validate
 from router.demo import demo
 
+
 load_dotenv(verbose=True)
 
 ROOT_PATH = str(os.getenv("ROOT_PATH"))
 
 app = FastAPI(root_path=ROOT_PATH)
-
 
 origins = [
     "http://localhost",
@@ -45,6 +45,11 @@ app.include_router(payment.router)
 app.include_router(validate.router)
 app.include_router(demo.router)
 
+# Logging configuration
+LOGGING_CONFIG["formatters"]["default"]["fmt"] = "%(asctime)s [%(name)s] %(levelprefix)s %(message)s"
+LOGGING_CONFIG["formatters"]["access"][
+    "fmt"] = '%(asctime)s [%(name)s] %(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s'
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000,
+                reload=True)
